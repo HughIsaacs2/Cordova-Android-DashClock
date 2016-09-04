19c8da0d5b804bb95ab402f7903f1eac1f72ae3f
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2016-09-04 11:42:34 -0400

    bam
    bam

--- MyDashClockExtension.java
+++ MyDashClockExtension.java
@@ -1,12 +1,24 @@
 package com.hughisaacs2.cordova.plugins.androiddashclockplugin;
 
-import com.google.android.apps.dashclock.api.DashClockExtension;
-import com.google.android.apps.dashclock.api.ExtensionData;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
 
 public class MyDashClockExtension extends DashClockExtension {
     private static final String TAG = "MyDashClockExtension";
@@ -17,7 +29,7 @@ public class MyDashClockExtension extends DashClockExtension {
         // Publish the extension data update.
         publishUpdate(new ExtensionData()
 				.visible(true)
-                .icon(R.drawable.icon)
+                //.icon(R.drawable.icon)
                 .status("Acrylic Style")
                 .expandedTitle("Acrylic Style Dot Com")
                 .expandedBody("Go to our site")
