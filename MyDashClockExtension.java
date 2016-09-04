b826548a083f1a2ba77179d8426a408f3c79a127
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2016-09-04 10:47:40 -0400

    One
    one

---
+++ MyDashClockExtension.java
@@ -0,0 +1,27 @@
+package com.hughisaacs2.cordova.plugins.androiddashclockplugin;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+
+public class MyDashClockExtension extends DashClockExtension {
+    private static final String TAG = "MyDashClockExtension";
+
+    @Override
+    protected void onUpdateData(int reason) {
+
+        // Publish the extension data update.
+        publishUpdate(new ExtensionData()
+				.visible(true)
+                .icon(R.drawable.icon)
+                .status("Acrylic Style")
+                .expandedTitle("Acrylic Style Dot Com")
+                .expandedBody("Go to our site")
+				.contentDescription("Go to AcrylicStyle.com")
+                .clickIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("http://acrylicstyle.com/"))));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Cordova-Android-DashClock
-Cordova Android DashClock Plugin
+Android DashClock Plugin for Cordova
+==============================
+
+I'm far from done, so this isn't to be used by anyone yet. I'm just sharing the code in case you wish to tear it apart and build you own or whatever.
+
+License
+-------
+
+This has been released under MIT license; see LICENSE for details.
